307c52800ca905c43e34f04da01405a9e387ec7d
Dadihans23 <Dadihans06@gmail.com>
2025-11-19 15:28:00 +0100

    second commit

--- money_transfer/settings.py
+++ money_transfer/settings.py
@@ -186,9 +186,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+
+# AJOUT OBLIGATOIRE pour collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'
